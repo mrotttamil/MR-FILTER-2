3fd286f5915e63a63cd1e963857b1596722650ff
mrotttamil <90447292+mrotttamil@users.noreply.github.com>
2022-07-05 14:12:36 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -92,7 +92,7 @@ def get_reply_markup(query):
     buttons = [
         [
             InlineKeyboardButton('Search again', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🗂 Files', url='t.me/MoviesFlixers_DL')
+            InlineKeyboardButton('🗂 Files', url='t.me/MR_OTT_Tamil2')
         ]
         ]
     return InlineKeyboardMarkup(buttons)
